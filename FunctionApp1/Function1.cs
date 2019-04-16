c041a4729a1c8f998652ef586c6cabdb86e7f084
p-manesh <p_manesh@hotmail.com>
2019-04-16 13:40:37 -0500

    Update Function1.cs

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -10,7 +10,7 @@ namespace FunctionApp1
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("Function2")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
@@ -29,7 +29,7 @@ namespace FunctionApp1
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+                : req.CreateResponse(HttpStatusCode.OK, "Hi " + name);
         }
     }
 }
