79ee7d2af7148760f2ad64e6b20389971618fa4f
p-manesh <p_manesh@hotmail.com>
2019-04-16 14:19:18 -0500

    Update Function1.cs

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -10,7 +10,7 @@ namespace FunctionApp1
 {
     public static class Function1
     {
-        [FunctionName("Test1")]
+        [FunctionName("Test2")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
